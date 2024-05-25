3c74a1eb51bcb88fdde6c585a3a9c5574485932b
Vickey <16484526+vickey-dev@users.noreply.github.com>
2024-05-25 08:49:09 +0530

    Updated the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'http://vickey-dev.github.io/scroll-themes-overlayScrollbars'
+  base:'https://vickey-dev.github.io/scroll-themes-overlayScrollbars'
 })
